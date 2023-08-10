a1c02299ac34ca5e30c00b019d747104680d97b5
Amirhosseinpourjon <131782746+am13ir84@users.noreply.github.com>
2023-08-10 14:02:42 +0330

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+function Number(num1, num2, num3, num4,num5, num6) 
+{var num7 = num1+num4;
+    var num8 = num2+num5;
+    var num9 = num3+num6;
+}
+var num1 = (prompt("Enter First number:"));
+var num2 = (prompt("Enter second number:"));
+var num3 = (prompt("Enter three number:"));
+var num4 = (prompt("Enter four number:"));
+var num5 = (prompt("Enter five number:"));
+var num6 = (prompt("Enter six number:"));
+alert(num7, num8, num9);
+Number(num7, num8, num9);
